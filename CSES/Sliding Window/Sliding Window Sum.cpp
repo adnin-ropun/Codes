7eea423c17159a1e6c6212f0c90381714a342b58
adnin-ropun <adninzahid55@gmail.com>
2025-09-28 20:42:27 +0600

    8:42 PM, 9/28/2025

---
+++ CSES/Sliding Window/Sliding Window Sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define int long long
+#define fastio ios::sync_with_stdio(0); cin.tie(0);
+#define T int T; cin >> T; while(T--)
+using namespace std;
+using vec = vector<int>; using mat = vector<vec>;
+template<typename Q> istream& operator>>(istream& is, vector<Q>& v) { for(auto& x : v) is >> x; return is; }
+
+
+int32_t main() {
+    fastio
+        
+}
